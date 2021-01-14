bb7248f93a7505b11a943148b74d68bf589426ea
harshgoel05 <harshgoel05@gmail.com>
2021-01-14 16:07:36 +0530

    🥅 Add Error Handling

--- src/service/certificate-service.ts
+++ src/service/certificate-service.ts
@@ -1,14 +1,18 @@
 import { Message } from "discord.js";
-import { certificateMessage } from "../utils/constants";
+import { certificateMessage, internalError } from "../utils/constants";
 import { getDbClient } from "../utils/database";
 import { ERRORS } from "../utils/errors";
 
 export async function getUserCertificate(incomingMessage: Message, email: any) {
-  const dbClient = await getDbClient();
-  const found = await dbClient
-    .db()
-    .collection("tech-troduction")
-    .countDocuments({ email: email });
-  if (found) incomingMessage.channel.send(certificateMessage);
-  else incomingMessage.channel.send(ERRORS.CERTIFICATE_NOT_FOUND);
+  try {
+    const dbClient = await getDbClient();
+    const found = await dbClient
+      .db()
+      .collection("tech-troduction")
+      .countDocuments({ email: email });
+    if (found) incomingMessage.channel.send(certificateMessage);
+    else incomingMessage.channel.send(ERRORS.CERTIFICATE_NOT_FOUND);
+  } catch (err) {
+    incomingMessage.channel.send(internalError);
+  }
 }
