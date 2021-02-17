ecfdc5ecf2053841e19bfd5592fe236e93f8134d
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-02-17 17:45:34 +0530

    🐛 fix react on command message

--- index.ts
+++ index.ts
@@ -72,10 +72,10 @@ async function createServer() {
             );
           }
         }
+        message.react(process.env.CUSTOM_EMOJI_ID!).catch((err) => {
+          serverLogger("non-fatal-error", "Could not find custom emoji", err);
+        });
       }
-      message.react(process.env.CUSTOM_EMOJI_ID!).catch((err) => {
-        serverLogger("non-fatal-error", "Could not find custom emoji", err);
-      });
     }
   });
 
