680a0038c24615d04a207150da5b83809fa0a214
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-02-19 18:24:11 +0530

    ✨ Notifications Request Schema

---
+++ src/api/notifications/notifications.schema.ts
@@ -0,0 +1,11 @@
+import * as yup from "yup";
+import { emailSchema } from "../../models/email";
+
+export const notificationsRequestSchema = yup.object({
+  emails: yup.array().of(emailSchema).required(),
+  slug: yup.string().required(),
+  subject: yup.string().required(),
+  body: yup.string().required(),
+});
+
+export type notificationsRequest = yup.InferType<typeof notificationsRequestSchema>;
